6544fa9abbbe1913d6198c22cb2ec8346e844a9a
Mathula <mathula2504@gmail.com>
2025-01-14 22:34:23 +0000

    Add Google site verification meta tag in layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
+        <meta name="google-site-verification" content="JMPQj5p4_AhcNoVk2wV0b5S3_1dLB8N9IAahKrmMd54" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
